d80ccea6c6daf41dc3f94024bfb012e3387abb82
aj-cpp <ajujun@cpp.edu>
2022-10-23 20:13:59 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+// CS2600 - Andrew Jujun
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  // Outline:
+  // Function that checks if it's a Lo Shu Magic Array
+  // -> Checks each rows, columns, diagonals if == 15
+  // if yes, then it's the magic array.
+
+  return EXIT_SUCCESS;
+}
